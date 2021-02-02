568701ea509d41b2c8f8058dba4713eaa458378c
Sam Toll <sam.x.toll@gmail.com>
2021-02-01 19:14:35 -0800

    fixed constant spinner

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -12,9 +12,10 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
   return (
     <div className="profile-github">
       <h2 className="text-primary my-1">Github Repos</h2>
-      {repos === null ? (
-        <Spinner />
-      ) : (
+      {
+        // repos === null ? (
+        //   <Spinner />
+        // ) : (
         repos.map((repo) => (
           <div key={repo._id} className="repo bg-white p-1 my-1">
             <div>
@@ -42,7 +43,7 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
             </div>
           </div>
         ))
-      )}
+      }
     </div>
   );
 };
