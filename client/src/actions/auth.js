4c1362de01fa77a2f34795c00255db8126886b27
Sam Toll <sam.x.toll@gmail.com>
2021-02-01 19:03:39 -0800

    fix small error

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -33,7 +33,7 @@ export const loadUser = () => async (dispatch) => {
 };
 
 // Register User
-export const register = ({ name, email, password }) => async (dispatch) => {
+export const register = (name, email, password) => async (dispatch) => {
   const config = {
     headers: {
       'Content-Type': 'application/json',
