73365fe61ab9a23f12b2381d4fb218f4f2ab2373
stoll2882 <sam.x.toll@gmail.com>
2021-02-07 23:02:59 -0500

    Added empty comment

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,6 +8,7 @@ const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
+//
 // @route    GET api/auth
 // @desc     Get user by token
 // @access   Private
